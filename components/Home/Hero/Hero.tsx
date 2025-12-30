821d9b7155ad882a223e49d3afb2802d8ae17390
fmfahath <fmfahath.dev@gmail.com>
2025-12-31 01:51:51 +0530

    feat(hero): input filed added

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -4,8 +4,18 @@ const Hero = () => {
     return (
         <div className='w-[95%] min-h-screen max-auto mt-[20vh] relative'>
             <div className='relative z-10 text-white flex flex-col items-center justify-center'>
+                {/* text content */}
                 <h1 className='text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center bg-gradient-to-r from-pink-600 to-blue-500 bg-clip-text text-transparent'>Create Beautiful Image with <br /> Artificial Intelligence</h1>
                 <p className='mt-3 text-sm md:text-base font-semibold text-center text-gray-300'>Get Started with our AI-Powered Image Generator tool</p>
+
+                {/* input field & button */}
+                <div className='h-11 md:h-16 w-[95%] sm:w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%] bg-white rounded-lg mt-12 px-2 md:px-6 flex items-center justify-center'>
+                    <input
+                        type="text"
+                        placeholder='Generate your dream Image...'
+                        className='w-full h-full rounded-lg outline-none text-black block flex-1 placeholder:text-sm md:placeholder:text-base'
+                    />
+                </div>
             </div>
         </div>
     )
