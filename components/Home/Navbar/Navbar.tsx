5b751b02692f8867f3647412f9816a25a3b9ec2b
fmfahath <fmfahath.dev@gmail.com>
2025-12-30 23:09:50 +0530

    feat(navbar): Navbar component complted

--- components/Home/Navbar/Navbar.tsx
+++ components/Home/Navbar/Navbar.tsx
@@ -1,8 +1,31 @@
+import { NavLinks } from '@/constant/constant'
+import { MenuIcon, SearchIcon, ShoppingCartIcon } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='flex items-center justify-between w-full h-[12vh] mx-auto z-[100] transition-all duration-200'>
+            {/* logo */}
+            <h1 className='text-3xl font-bold text-white ml-8 md:ml-16'>PIXA</h1>
+
+            <div className='md:flex items-center space-x-10 hidden'>
+                {/* navlinks */}
+                {NavLinks.map(link => (
+                    <Link href={link.url} key={link.id} className='relative text-white text-base w-fit block after:block after:content-[""] 
+                    after:absolute after:h-[3px] after:bg-yellow-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after::origin-center'>
+                        <p>{link.label}</p>
+                    </Link>
+                ))}
+            </div>
+
+            {/* nav icons */}
+            <div className='flex items-center space-x-5 md:space-x-8 text-white mr-8 md:mr-16'>
+                <ShoppingCartIcon className='cursor-pointer w-6 h-6' />
+                <SearchIcon className='cursor-pointer w-6 h-6' />
+                <MenuIcon className='cursor-pointer w-6 h-6 md:hidden' />
+            </div>
+        </div>
     )
 }
 
