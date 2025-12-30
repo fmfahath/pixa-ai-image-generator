7884dfca9340993cf2271cbeb7532c8158f2a0bc
fmfahath <fmfahath.dev@gmail.com>
2025-12-30 22:40:40 +0530

    chore(component): Navbar, MobileNav, responsive component created and root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from 'next/font/google';
 import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 
 const roboto = Roboto({
   weight: ['100', '300', '400', '500', '700', '900'],
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${roboto.className} bg-gray-900 antialiased`}>
+        <ResponsiveNav />
         {children}
       </body>
     </html>
---
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MobileNav from './MobileNav'
+import Navbar from './Navbar'
+
+const ResponsiveNav = () => {
+    return (
+        <div>
+            <MobileNav />
+            <Navbar />
+        </div>
+    )
+}
+
+export default ResponsiveNav
