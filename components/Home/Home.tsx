1ee3b5bdf747f5eafff45386e17d81882b6af306
fmfahath <fmfahath.dev@gmail.com>
2026-01-04 23:29:00 +0530

    feat(recent-img-slider): header section added

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import RecentImages from './RecentImages/RecentImages'
 
 const Home = () => {
 
     return (
         <div className='overflow-hidden'>
             <Hero />
+            <RecentImages />
         </div>
     )
 }
