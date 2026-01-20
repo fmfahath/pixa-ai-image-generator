fa31d7bcb8dc481052203ba534a0ee47734af5f6
fmfahath <fmfahath.dev@gmail.com>
2026-01-20 23:26:00 +0530

    feat(faq-section): faqCard compnent created, accordian shadcn npm installed

--- components/Home/Faq/FaqCard.tsx
+++ components/Home/Faq/FaqCard.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
-const FaqCard = () => {
+
+export function FaqCard() {
     return (
-        <div>FaqCard</div>
+        <Accordion type="single" collapsible defaultValue="item-1" className="text-white">
+            <AccordionItem value="item-1">
+                <AccordionTrigger className="text-xl lg:text-2xl">Is it accessible?</AccordionTrigger>
+                <AccordionContent className="text-base md:text-lg">
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+                <AccordionTrigger className="text-xl lg:text-2xl">Is it accessible?</AccordionTrigger>
+                <AccordionContent className="text-base md:text-lg">
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+                <AccordionTrigger className="text-xl lg:text-2xl">Is it accessible?</AccordionTrigger>
+                <AccordionContent className="text-base md:text-lg">
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion>
     )
 }
 
